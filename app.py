d5c2912a4fb374f1b54017d78c7978db4964ef5c
Kennedy Rotich <132645931+RotichKipkoech@users.noreply.github.com>
2024-11-12 13:05:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out.', 'success')
+    flash('Bye! You have been logged out.', 'success')
     return redirect(url_for('login'))
 
 @app.route('/admin_dashboard')
